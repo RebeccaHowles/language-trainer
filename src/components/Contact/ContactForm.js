c49defb0c88638261d3f10e05f6d1331e8a344cb
Tiffany Santos <>
2021-01-05 17:59:40 +0100

    contact-form

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,36 +1,10 @@
 import React , {useState, useEffect } from "react";
 
-
-const handleSubmit = (e) => {
-  e.preventDefault()
-  let myForm = document.getElementById('contact-form');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Form successfully submitted')).catch((error) =>
-    alert(error))
-}
-
-document.querySelector("form").addEventListener("submit", handleSubmit);
-
-
 export function ContactForm() {
 
-  const [success, setSuccess] = useState(false);
-  useEffect(() => {
-    if ( window.location.search.includes('success=true') ) {
-      setSuccess(true);
-    }
-  }, []);
-
   return (
-      <form name="contact-form" id='contact-form' method="POST" data-netlify-recaptcha="true" data-netlify="true" action="/contact/?success=true">
+      <form name="contact-form" id='contact-form' method="POST" data-netlify-recaptcha="true" data-netlify="true">
         <input type="hidden" name="contact-form" value="contact-form" />
-        {success && (
-          <p style={{ color: "green" }}>Message succesfully sent! </p>
-        )}
       <p>
         <label for="name">Name</label>
         <input type="text" id="name" name="name" />
