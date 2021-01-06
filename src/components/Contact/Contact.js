6831e5010517d1f7f21aeb1628d0a34f062c99d7
Tiffany Santos <>
2021-01-06 15:13:01 +0100

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,56 +1,32 @@
 /* eslint-disable no-unused-vars */
 import './contact.css';
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { useForm } from 'react-hook-form';
 
-
-  
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
 export function Contact () {
 
-  const { register, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm();
 
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [message,setMessage] = useState('');
-  const [status,setStatus] = useState('');
+  const [notification, setNotification] = useState('');
 
-  const handleChange = e => {
-    const {name, value} = e.target
-    if (name === 'name' ){
-      return setName(value)
-    }
-    if (name === 'email' ){
-      return setEmail(value)
-    }
-    if (name === 'message' ){
-      return setMessage(value)
-    }
-  }
-
-  const encode = (data) => {
-    const formData = new FormData();
-    Object.keys(data).forEach((k)=>{
-      formData.append(k,data[k])
-    });
-    return formData
-  }
-
-  const handleSubmit = e => {
-    const data = { "form-name": "contact", name, email, message, file }
-    
+  const onSubmit = data => {
+    //alert(JSON.stringify(data));
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode(data)
+      body: encode({ "form-name": "contact", ...data })
     })
-      .then(() => setStatus("Thank you for your message!"))
-      .catch(error => setStatus("Sorry, message not sent!"));
-
-    e.preventDefault();
-  };
+      .then(() => setNotification("Form Submission Successful!!"))
+      .catch(error => setNotification("Form Submission Failed!")); 
+  }; 
 
   return (
     <div className='content'>
@@ -60,42 +36,39 @@ export function Contact () {
         <p>Contact via <FontAwesomeIcon icon={faPhone} />{' '}
           <span className='iconify' data-icon='fa-whatsapp' data-inline='false'/>{' '}+49(0)1737586025</p>
           <p>or message me directly:</p>
-          <h2>{status}</h2>
+          <h2>{notification}</h2>
           <form name="contact" 
             method="POST" 
-            action="/thank-you"
+            action="/thank-you/"
             data-netlify="true" 
             data-netlify-recaptcha="true"
-            onSubmit={handleSubmit}>
+            onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control">
-              <label>Name</label>
-              <input type="text" name="Name" ref={register({ required: true})} />
-              {errors.Name && <span>This field is required</span>}
+              <label>Name {errors.name && <span>This field is required</span>}</label>
+              <input type="text" name="name" ref={register({ required: true})} />
             </div>
             <div className="form-control">
-              <label>Email</label>
-              <input type="text" name="Email" ref={register({
+              <label>Email { errors.email && <span>Please enter a valid email</span>}</label>
+              <input type="text" name="email" ref={register({
               required: true,
               pattern: {
               value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
               message: "invalid email address"
               }
             })} />
-            { errors.Email && <span>Please enter a valid email</span>}
             </div>
             <div className="form-control">
-              <label>Level</label>
-              <select name="Level" ref={register({ required: true })}>
+              <label>Level {errors.level && <span>Select your level</span>}</label>
+              <select name="level" ref={register({ required: true })}>
+              <option value=""></option>
                 <option value="Beginner">Beginner</option>
                 <option value="Intermediate">Intermediate</option>
                 <option value="Advanced">Advanced</option>
               </select>
-              {errors.Level && <span>This field is required</span>}
           </div>
             <div className="form-control">
-              <label>Message</label>
-              <textarea type="text" name="Message" ref={register({ required: true})} />
-              {errors.Message && <span>This field is required</span>}
+              <label>Message {errors.message && <span>This field is required</span>}</label>
+              <textarea type="text" name="message" ref={register({ required: true})} />
             </div>   
             <div data-netlify-recaptcha="true"></div>
             <div className="form-control">
