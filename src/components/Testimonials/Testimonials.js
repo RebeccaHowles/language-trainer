0c7299040209d6b6ae9e25b2ca4b84cda3c0c35c
RebeccaHowles <77383239+RebeccaHowles@users.noreply.github.com>
2021-03-01 11:16:11 +0100

    reordered quotes

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -7,15 +7,15 @@ export function Testimonials () {
   const Testimonials = [
     {
       id: '1',
-      quote: 'Una profesora excelente cuyas clases son muy amenas. Se adapta a las necesidades de los alumnos, tanto de nivel como de intereses. 100% aconsejable.',
-      name: 'Frederica',
-      year: '11 jun 2020'
+      quote: 'Es una profesora excelente, hace las clases muy amenas con muy buenos materiales y te da muchísima confianza para que hables y te sueltes en inglés. En pocos meses hemos avanzado muchísimo además de conseguir una motivación extra! 100% recomendada!!',
+      name: 'Carmen',
+      year: '03 abr 2017'
     },
     {
       id: '2',
-      quote: 'Rebecca ha sido una profesora muy buena. Sus clases son dinámicas y participativas. Se implica mucho contigo.',
-      name: 'Ana',
-      year: '12 feb 2019'
+      quote: 'Es una profesora muy profesional y cualificada, siempre tiene preparadas las clases, son muy buenas y se aprende mucho, combina el aprendizaje con la diversión. Es feliz dando las clases, siempre con una sonrisa y eso se transmite al alumno.',
+      name: 'Daniel',
+      year: '27 mar 2017'
     },
     {
       id: '3',
@@ -25,15 +25,15 @@ export function Testimonials () {
     },
     {
       id: '4',
-      quote: 'Es una profesora excelente, hace las clases muy amenas con muy buenos materiales y te da muchísima confianza para que hables y te sueltes en inglés. En pocos meses hemos avanzado muchísimo además de conseguir una motivación extra! 100% recomendada!!',
-      name: 'Carmen',
-      year: '03 abr 2017'
+      quote: 'Rebecca ha sido una profesora muy buena. Sus clases son dinámicas y participativas. Se implica mucho contigo.',
+      name: 'Ana',
+      year: '12 feb 2019'
     },
     {
       id: '5',
-      quote: 'Es una profesora muy profesional y cualificada, siempre tiene preparadas las clases, son muy buenas y se aprende mucho, combina el aprendizaje con la diversión. Es feliz dando las clases, siempre con una sonrisa y eso se transmite al alumno.',
-      name: 'Daniel',
-      year: '27 mar 2017'
+      quote: 'Una profesora excelente cuyas clases son muy amenas. Se adapta a las necesidades de los alumnos, tanto de nivel como de intereses. 100% aconsejable.',
+      name: 'Frederica',
+      year: '11 jun 2020'
     },
     {
       id:'6',
